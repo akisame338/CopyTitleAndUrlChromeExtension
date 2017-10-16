28844a19d48a0442ae429fbefd3b0c6c524b94e6
akisame338 <akisame338@gmail.com>
2017-10-17 00:47:40 +0900

    Docコメントを記載

--- js/background.js
+++ js/background.js
@@ -4,10 +4,22 @@
     document.body.appendChild(textarea);
 }());
 
+/**
+ * クリップボードにコピーする内容を生成
+ *
+ * @param  {String} title ページのタイトル
+ * @param  {String} url   ページのURL
+ * @return {String} クリップボードにコピーする内容
+ */
 function createContent(title, url) {
     return `${title}\n${url}`;
 }
 
+/**
+ * 引数で渡された内容をクリップボードにコピー
+ *
+ * @param {String} content クリップボードにコピーする内容
+ */
 function copyToClipboard(content) {
     var textarea = document.getElementById('textarea');
     textarea.value = content;
@@ -15,15 +27,26 @@ function copyToClipboard(content) {
     document.execCommand('copy');
 }
 
+/**
+ * 引数で指定されたタブで表示しているページのタイトルとURLをクリップボードにコピー
+ *
+ * @param {Tab} カレントウィンドウのアクティブなタブ
+ */
 function copyTitleAndUrlToClipboard(tab) {
     var content = createContent(tab.title, tab.url);
     copyToClipboard(content);
 }
 
+/**
+ * アイコンクリック時のイベント
+ */
 chrome.browserAction.onClicked.addListener(function(tab) {
     copyTitleAndUrlToClipboard(tab);
 });
 
+/**
+ * ショートカットキー入力時のイベント
+ */
 chrome.commands.onCommand.addListener(function(command) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         copyTitleAndUrlToClipboard(tabs[0]);
